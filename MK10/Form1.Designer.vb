7f42de0e9c090714bd4dc7f01a198b28899e08dd
Mohd Irfan Izzuddin Bin Jamaluddin <18DDT22F1032@student.ptss.edu.my>
2024-10-14 12:59:10 +0800

    Add project files.

---
+++ MK10/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    Private components As System.ComponentModel.IContainer
+
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        txtIdPelajar = New TextBox()
+        txtNamaPelajar = New TextBox()
+        txtNoMatrik = New TextBox()
+        cmbKelas = New ComboBox()
+        btnAdd = New Button()
+        dataGridView1 = New DataGridView()
+        Timer1 = New Timer(components)
+        CType(dataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtIdPelajar
+        ' 
+        txtIdPelajar.Location = New Point(20, 30)
+        txtIdPelajar.Name = "txtIdPelajar"
+        txtIdPelajar.PlaceholderText = "Enter ID Pelajar"
+        txtIdPelajar.Size = New Size(542, 27)
+        txtIdPelajar.TabIndex = 0
+        ' 
+        ' txtNamaPelajar
+        ' 
+        txtNamaPelajar.Location = New Point(20, 70)
+        txtNamaPelajar.Name = "txtNamaPelajar"
+        txtNamaPelajar.PlaceholderText = "Enter Nama Pelajar"
+        txtNamaPelajar.Size = New Size(542, 27)
+        txtNamaPelajar.TabIndex = 1
+        ' 
+        ' txtNoMatrik
+        ' 
+        txtNoMatrik.Location = New Point(20, 110)
+        txtNoMatrik.Name = "txtNoMatrik"
+        txtNoMatrik.PlaceholderText = "Enter No Matrik"
+        txtNoMatrik.Size = New Size(542, 27)
+        txtNoMatrik.TabIndex = 2
+        ' 
+        ' cmbKelas
+        ' 
+        cmbKelas.DropDownStyle = ComboBoxStyle.DropDownList
+        cmbKelas.FormattingEnabled = True
+        cmbKelas.Location = New Point(20, 150)
+        cmbKelas.Name = "cmbKelas"
+        cmbKelas.Size = New Size(542, 28)
+        cmbKelas.TabIndex = 3
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(575, 30)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(146, 27)
+        btnAdd.TabIndex = 4
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' dataGridView1
+        ' 
+        dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dataGridView1.Location = New Point(20, 190)
+        dataGridView1.Name = "dataGridView1"
+        dataGridView1.RowHeadersWidth = 51
+        dataGridView1.Size = New Size(701, 150)
+        dataGridView1.TabIndex = 7
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 1000
+        ' 
+        ' Form1
+        ' 
+        ClientSize = New Size(743, 356)
+        Controls.Add(dataGridView1)
+        Controls.Add(btnAdd)
+        Controls.Add(cmbKelas)
+        Controls.Add(txtNoMatrik)
+        Controls.Add(txtNamaPelajar)
+        Controls.Add(txtIdPelajar)
+        Name = "Form1"
+        Text = "Pelajar Management"
+        CType(dataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtIdPelajar As TextBox
+    Friend WithEvents txtNamaPelajar As TextBox
+    Friend WithEvents txtNoMatrik As TextBox
+    Friend WithEvents cmbKelas As ComboBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents dataGridView1 As DataGridView
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ MK10/Form1.vb
@@ -0,0 +1,93 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private conn As MySqlConnection
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conn = New MySqlConnection("Server=172.20.10.3;Database=mk9;Uid=username;Pwd=password;")
+        LoadKelas() ' Load classes into ComboBox
+        LoadData()
+    End Sub
+
+    Private Sub LoadKelas()
+        Dim query As String = "SELECT idkelas, namakelas FROM kelas"
+        Dim cmd As MySqlCommand = New MySqlCommand(query, conn)
+
+        Try
+            conn.Open()
+            Dim reader As MySqlDataReader = cmd.ExecuteReader()
+            cmbKelas.Items.Clear() ' Clear existing items
+            While reader.Read()
+                cmbKelas.Items.Add(New KeyValuePair(Of Integer, String)(reader("idkelas"), reader("namakelas")))
+            End While
+            reader.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub LoadData()
+        ' Use a JOIN query to retrieve both pelajar and kelas details
+        Dim query As String = "SELECT p.idpelajar, p.namapelajar, p.nomatrik, k.namakelas " &
+                          "FROM pelajar p " &
+                          "INNER JOIN kelas k ON p.idkelas = k.idkelas"
+
+        Dim cmd As MySqlCommand = New MySqlCommand(query, conn)
+        Dim adapter As New MySqlDataAdapter(cmd)
+        Dim table As New DataTable()
+
+        Try
+            conn.Open()
+            adapter.Fill(table)
+            dataGridView1.DataSource = table
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If cmbKelas.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select a class.")
+            Return
+        End If
+
+        Dim selectedKelas = CType(cmbKelas.SelectedItem, KeyValuePair(Of Integer, String)).Key
+
+        Dim query As String = "INSERT INTO pelajar (idpelajar, namapelajar, nomatrik, idkelas) VALUES (@idpelajar, @namapelajar, @nomatrik, @idkelas)"
+        Dim cmd As MySqlCommand = New MySqlCommand(query, conn)
+
+        cmd.Parameters.AddWithValue("@idpelajar", txtIdPelajar.Text)
+        cmd.Parameters.AddWithValue("@namapelajar", txtNamaPelajar.Text)
+        cmd.Parameters.AddWithValue("@nomatrik", txtNoMatrik.Text) ' Change this as needed
+        cmd.Parameters.AddWithValue("@idkelas", selectedKelas)
+
+        Try
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Pelajar added successfully.")
+            LoadData()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub dataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dataGridView1.CellClick
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow = dataGridView1.Rows(e.RowIndex)
+            txtIdPelajar.Text = row.Cells("idpelajar").Value.ToString()
+            txtNamaPelajar.Text = row.Cells("namapelajar").Value.ToString()
+            cmbKelas.SelectedItem = cmbKelas.Items.Cast(Of KeyValuePair(Of Integer, String))().FirstOrDefault(Function(c) c.Key = CInt(row.Cells("idkelas").Value))
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        LoadData()
+
+    End Sub
+End Class
